6b1565721bf084d438d11ccaa4e0bb9d3de90d49
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-09-11 23:17:16 -0800

    Add files via upload

--- greatest.cpp
+++ greatest.cpp
@@ -3,7 +3,6 @@
 //9/10/20
 //CS201
 
-
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -12,10 +11,12 @@ using std::endl;
 
 int main()
 {
-    cout<<"Enter a sequence of positive numbers ";
+    cout<<"Enter a sequence of positive numbers";
+    cout<<endl;
+
     int v=0, LN=0;// v is the values entered by the user LN will be the largest number at the end
 
-    cout<<"Enter a positive number(0 or negative to end): ";
+    cout<<"Enter a positive number(0 or negative to end):";
     cin>>v;
 
     while(v>0){ // as long as it is above 0 program continues
