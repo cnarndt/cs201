350451d213924d7d69e7e060c15dd388bde8367f
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-09-11 22:08:27 -0800

    Add files via upload

---
+++ greatest.cpp
@@ -0,0 +1,32 @@
+//Clayton Arndt
+// HW1
+//9/10/20
+//CS201
+
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+    cout<<"Enter a sequence of positive numbers ";
+    int v=0, LN=0;// v is the values entered by the user LN will be the largest number at the end
+
+    cout<<"Enter a positive number(0 or negative to end): ";
+    cin>>v;
+
+    while(v>0){ // as long as it is above 0 program continues
+        if(v>LN)
+            LN=v;
+        cout<<"Enter a positive number(0 or negative to end):";
+        cin>>v;
+    }//end while loop
+
+    cout<<"Largest number is: "<<LN<<'\n'<<endl; //after the while loop the LN is displayed here
+
+
+    return 0;
+}
---
+++ mileskm.cpp
@@ -0,0 +1,17 @@
+//Clayton Arndt
+// HW1
+//9/10/20
+//CS201
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int i; // Miles
+    cout<<"\n\nEnter number of miles: ";
+    cin>>i;
+    cout <<"Kilometers:" << (i*1.609);
+
+}
